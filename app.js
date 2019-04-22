8bb118d4b058b557670118d46ac4ea0a7af61e7c
Ramon Davi <ramondavi@msn.com>
2019-04-22 18:21:22 -0300

    added some basic elements of each pages

--- app.js
+++ app.js
@@ -12,4 +12,12 @@ app.get("/inicio", (req, res) =>
     res.render("index")
 );
 
+app.get("/info", (req, res) =>
+    res.render("info")
+);
+
+app.get("/revise", (req, res) =>
+    res.render("topicos")
+);
+
 app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
