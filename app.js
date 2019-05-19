36d3952af1547cb761e8c398a21bd232cf2b4798
Ramon Davi <ramondavi@msn.com>
2019-05-19 09:15:27 -0300

    minor changes 3

--- app.js
+++ app.js
@@ -461,4 +461,7 @@ app.post('/forgot', function(req, res, next) {
   });
 
 
-app.listen(3000, () => console.log(`Example app listening on port ${3000}!`));
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+      console.log(`Our app is running on port ${ PORT }`);
+  });
